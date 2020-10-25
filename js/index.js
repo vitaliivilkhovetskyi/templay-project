a924db3493ad560f7f16a75cd529b58fa2ffac3e
vitaliivilkhovetskyi <vilkhovetskyivitalii@users.noreply.github.com>
2020-10-25 19:26:39 +0200

    added js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,35 @@ function myFunction(x) {
   x.classList.toggle("change");
 }
 
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 1000,
+    easing: 'linear',
+    autoplay: true,
+    autoplaySpeed: 1000,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    draggable: false,
+    waitForAnimate: false,
+    centerMode: false,
+    variableWidth: false,
+    rows: 1,
+    slidesPerRow: 1, 
+    vertical: false,
+    verticalSwiping: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow:2,
+        }
+      }
+    ]
+  });
+});
+
